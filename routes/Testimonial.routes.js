003d62b983f610da035257ca541e05cb6256305d
Sumit-Chouhan-786 <156494276+Sumit-Chouhan-786@users.noreply.github.com>
2025-09-24 17:07:21 +0530

    Update Testimonial.routes.js
    update backend testimonial

--- routes/Testimonial.routes.js
+++ routes/Testimonial.routes.js
@@ -9,7 +9,7 @@ const upload = multer({ storage });
 
 TestimonialRoute.post("/", upload.single("Image"), addTestimonial);
 TestimonialRoute.put("/:id", upload.single("Image"), updateTestimonial);
-TestimonialRoute.get("/", getTestimonials);
+TestimonialRoute.get("/:id", getTestimonials);
 TestimonialRoute.delete("/:id", deleteTestimonial);
 
 export default TestimonialRoute;
