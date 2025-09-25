3e7460aa4f4d43d3c013b0d8a1e184d838856e73
Sumit-Chouhan-786 <156494276+Sumit-Chouhan-786@users.noreply.github.com>
2025-09-25 14:58:03 +0530

    Update Dc.js
    required some fields

--- models/Dc.js
+++ models/Dc.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const dcSchema = new mongoose.Schema({
-    name:{type:String},
-    email:{type:String},
-    number:{type:Number},
+    name: { type: String, required: true },
+  email: { type: String, required: true },
+  number: { type: String, required: true }, 
     district:{type:String},
     state:{type:String},
     address:{type:String},
