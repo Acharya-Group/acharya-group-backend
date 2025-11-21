c930b6cf49b08ef6773d97f99778e48639bca9a8
Acharya Group <acharyasumit786@gmail.com>
2025-11-21 13:56:18 +0530

    updated backend in progress

--- index.js
+++ index.js
@@ -7,23 +7,28 @@ import morgan from 'morgan'
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8006;
 
-// ✅ CORS: allow frontend domains and localhost
-const allowedOrigins = [
-  "https://acharya-group.vercel.app",
-  "https://achariyagroup.in",
-  "http://localhost:3000",
-  "https://acharya-group-six.vercel.app"
-];
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+  "https://achariyagroup.in"
+  ],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+}));
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+
+app.use((req, res, next) => {
+  // res.header("Access-Control-Allow-Origin", "https://achariyagroup.in");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -36,6 +41,6 @@ app.use("/api/v1", routes);
 ConnectDb();
 
 // ✅ Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
